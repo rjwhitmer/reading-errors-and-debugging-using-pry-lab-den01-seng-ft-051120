9c5f34bc4e17a365187122b5cc10ee6ce367b021
dapper-module-0474 <rjwhitmercc@gmail.com>
2020-04-24 23:05:02 +0000

    Done.

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -1,15 +1,27 @@
 # don't forget to add: require 'pry'
 
+require 'pry'
+
 def generate_star_date
-  (rand(100000) + 400000) / 10.0
+  star_date = (rand(100000) + 400000) / 10.0
+  star_date
 end
 
+#star_date = generate_star_date
 
-def state_log(star_date)
-  "Captain's Log, star date #{star_date}."
+def captains_log(star_date)
+  announcement = "Captain's Log, star date #{star_date}."
+  announcement
+  #binding.pry
 end
+
+#state_log = captains_log(star_date)
 
 def engage
-  puts state_log(date)
-  date = generate_star_date
+  star_date = generate_star_date
+  #state_log = captains_log(star_date)
+  puts "Captain's Log, star date "
+  #crew_greetings
+  #binding.pry
 end
+#binding.pry
